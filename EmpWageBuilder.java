76dc6d1c90785b0d2a608d2e8bb9b55c7ce68943
Sandypandit <sandypandit86@gmail.com>
2021-10-20 02:20:47 +0530

    checking wage condition using OOPS concept

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,26 +1,69 @@
 package com.bridgelabz;
 import java.util.Random;
 
-class EmpWagebuilder {
+public class EmpWageBuilder {
+	public static final int WagePerHour = 20;
+	public static final int workDays = 20;
+	int monthlyWage=0, workHours=0;
+	int monthlyWorkHours=0;
+	int presentdays = 0;
 	String name;
 	int empID;
-
-	public void attendance() {
-		Random rd = new Random(2);
-		int a = rd.nextInt();
-		if(a==1) { System.out.println("Employee is present"); }
-		else { System.out.println("Employee is absent"); }
+	int p=1;
+	
+	public int dailyWage() { // daily wage calculator
+		Random rd = new Random();
+		int attendance = rd.nextInt(2);
+		
+		if(attendance == 0){ 
+			workHours = 8; //present
+            System.out.printf("Day %d: P, ", p++);
+            presentdays++;
+		}
+		
+        else if(attendance == 1){ 
+			workHours = 0;//absent
+            System.out.printf("Day %d: A, ", p++); 
+		}
+		
+		int DailyWage = WagePerHour*workHours;
+		
+		return DailyWage;
 	}
 	
-	public EmpWagebuilder(String name, int empID) {
+	public void monthlyWageCond() {
+		//Monthly wage Calculation
+
+		for (int i=1;i<=workDays;i++) { 
+			monthlyWage += dailyWage();
+			//daily calculations of work hours
+			monthlyWorkHours+=workHours;
+		}
+		
+		if (monthlyWorkHours==100 && presentdays==20) {
+			System.out.println("\nMonthly wage of employee is Rs " + monthlyWage+"\n");
+		}
+		else {
+			System.out.println("\nIncomplete working Hours or Days\n"
+					+ "Whole month working Hours = "+monthlyWorkHours+
+					"\nWhole month working days = "+presentdays);
+		}
+	}
+	
+	public EmpWageBuilder(String name, int empID) { //Constructor
 		this.name = name;
 		this.empID = empID;
-		System.out.println(name+"\n"+empID);
+		System.out.println(this.name+"\n"+this.empID);
 	}
-	public static void main(String[] args){
-		//Empwagebuilder
-		EmpWagebuilder emp1 = new EmpWagebuilder("Sandesh", 2347);
-		emp1.attendance();
-		System.out.println(emp1);
+
+	public static void main(String[] args) {
+		
+		EmpWageBuilder emp1 = new EmpWageBuilder("Sandesh", 2347);
+		EmpWageBuilder emp2 = new EmpWageBuilder("Swapnil", 2098);
+
+		emp1.monthlyWageCond();
+		
+		emp2.monthlyWageCond();
 	}
+
 }
